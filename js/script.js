a80bb7884dec235170fd465ecb96769a2ae6bced
Riulsudario <rsudario47@gmail.com>
2020-03-24 08:21:36 -0400

    50% pronto
    Muitas coisas foram alteradas, como novas seções, navbar, texto introdutório, cards de skills e etc

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const go = (elem) => {
+    window.scroll({      
+        top: document
+      .querySelector( elem )
+        .offsetTop,      
+        left: 0,
+        behavior: 'smooth'
+     });
+  }
+
